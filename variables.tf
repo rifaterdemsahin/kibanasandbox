b6a03691cf43db6c508497fdfaca3afec97df41f
Erdem <rifaterdemsahin@gmail.com>
2024-08-11 12:56:07 +0100

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "do_token" {
+  description = "DigitalOcean API token"
+  type        = string
+}
+
+variable "region" {
+  description = "The region to deploy the resources in"
+  type        = string
+  default     = "nyc3"
+}
+
+variable "droplet_size" {
+  description = "The size of the droplet"
+  type        = string
+  default     = "s-1vcpu-2gb"
+}
+
+variable "node_count" {
+  description = "The number of Kibana nodes"
+  type        = number
+  default     = 2
+}
+
+variable "ssh_key_id" {
+  description = "The SSH key ID to access the droplets"
+  type        = string
+}
